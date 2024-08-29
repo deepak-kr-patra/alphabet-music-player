3c277e51a6e54dce8167059f080907bbde78e3bb
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2024-08-29 14:09:33 +0530

    changed URLs of songs

--- src/script.js
+++ src/script.js
@@ -8,107 +8,107 @@ const run = (e) => {
     console.log(e);
     musicPrevious = music;
     if (e.key == "a" || e.key == "A") {
-        music.src = "https://audio.jukehost.co.uk/Ie7inuE9gDVKh8itNwyNGKnbbxWbMLfJ";
+        music.src = "https://audio.jukehost.co.uk/pWY9IYspaTneoePu9kZLJc0YXkmRmC2K";
         songname = "Aa Toh Sahi";
     }
     else if (e.key == "b" || e.key == "B") {
-        music.src = "https://audio.jukehost.co.uk/J6dKenWIb99lWgEX2HxM8tcmxvjjWmHu";
+        music.src = "https://audio.jukehost.co.uk/2o1hosORbUyiGr6rLXuQYHcSj7TtsarV";
         songname = "Brown Rang";
     }
     else if (e.key == "c" || e.key == "C") {
-        music.src = "https://audio.jukehost.co.uk/s2V6UNUX92zvXBf28GPg1vOVCfkIupmP";
+        music.src = "https://audio.jukehost.co.uk/6o53suYxfflRSTvzAXFQUhml0O24fmrU";
         songname = "Chudi Jo Khanke";
     }
     else if (e.key == "d" || e.key == "D") {
-        music.src = "https://audio.jukehost.co.uk/qvhlLueWRhcu78ToFf64zmMYfloEnHJj";
+        music.src = "https://audio.jukehost.co.uk/X8Tr4QAyCh29NLnwty3ZMy7Q0rgFzjCs";
         songname = "Dekha Ek Khwab";
     }
     else if (e.key == "e" || e.key == "E") {
-        music.src = "https://audio.jukehost.co.uk/Y81MMl0kqTdZvbIt2Yc2n8kObZTxItGs";
+        music.src = "https://audio.jukehost.co.uk/wDmWi642xyi6EOjRUTU31f95NoI84V3U";
         songname = "Ek Mulaqat";
     }
     else if (e.key == "f" || e.key == "F") {
         music.src = "../music-folder/.mp3";
-        songname = "https://audio.jukehost.co.uk/FAclW2tb4WmtBnZX9c8N6mCk8HuQinOI";
+        songname = "https://audio.jukehost.co.uk/zsOlHGGGk7XGppP3llKMwJRl31IN9o3B";
     }
     else if (e.key == "g" || e.key == "G") {
-        music.src = "https://audio.jukehost.co.uk/VYveym61ZXbVVYNzGlFkfVTK12D8Sxnj";
+        music.src = "https://audio.jukehost.co.uk/VD1Zf3xvWCurbDueKrpnecy0qp8k33gk";
         songname = "Ghungroo";
     }
     else if (e.key == "h" || e.key == "H") {
-        music.src = "https://audio.jukehost.co.uk/MHCapmXMEKQ3Vqw4y49EmwnG0agZLKB5";
+        music.src = "https://audio.jukehost.co.uk/4KFENOcgPbTCxZ4ETwlGq2BkzxPQucNy";
         songname = "Hawayein";
     }
     else if (e.key == "i" || e.key == "I") {
-        music.src = "https://audio.jukehost.co.uk/Zp4wJuEj0wyskIeVJ1cYN1jqat7zHnKK";
+        music.src = "https://audio.jukehost.co.uk/r8Ezopd56Hh2bqTr0Pgasjiq2LETWy34";
         songname = "Ik Vaari Aa";
     }
     else if (e.key == "j" || e.key == "J") {
-        music.src = "https://audio.jukehost.co.uk/uuRPceKc6dzo8Vpam1SODiX1OWdiCReM";
+        music.src = "https://audio.jukehost.co.uk/qTSv2ooUmtrOlrDUrzh4KqlqR9FFa4aN";
         songname = "Jab Tak";
     }
     else if (e.key == "k" || e.key == "K") {
-        music.src = "https://audio.jukehost.co.uk/B2bEUDyX43mphRcCbeagx850QfelGmV9";
+        music.src = "https://audio.jukehost.co.uk/0s9nYvJ2ubSNaAZLxTOXSgpKdFmkXN9x";
         songname = "Khairiyat";
     }
     else if (e.key == "l" || e.key == "L") {
-        music.src = "https://audio.jukehost.co.uk/FBfxTiI5HTnMlP4ZlNqNy4i5XI02wgFx";
+        music.src = "https://audio.jukehost.co.uk/GSl61BbJMQdjLOSOVVRMD4stVS6BzAHH";
         songname = "Let Me Love You";
     }
     else if (e.key == "m" || e.key == "M") {
-        music.src = "https://audio.jukehost.co.uk/Bmke1iYNiqHwBH5VzdyQF3tVIjKq4uzs";
+        music.src = "https://audio.jukehost.co.uk/5t1rQcGnBUnEzwt9a7Upf6I6eoRSu5lS";
         songname = "Mere Samne Wali Khidki";
     }
     else if (e.key == "n" || e.key == "N") {
-        music.src = "https://audio.jukehost.co.uk/4lLSTVIahngaagC0KGQzENBImQv8AeGF";
+        music.src = "https://audio.jukehost.co.uk/z1aCt736FuiovqGIlWxCPbLQGn7aTBFT";
         songname = "Na Na Na Na";
     }
     else if (e.key == "o" || e.key == "O") {
-        music.src = "https://audio.jukehost.co.uk/O6kz6TZVGytQuV7j913fqZVxNNFU2Pvf";
+        music.src = "https://audio.jukehost.co.uk/tFKGpoVqU2kH7PLUvcb3QSKk3l5rdWMW";
         songname = "O Saathi";
     }
     else if (e.key == "p" || e.key == "P") {
-        music.src = "https://audio.jukehost.co.uk/BaPQgUh20krlmwsdZFYjY10J6adPxlfT";
+        music.src = "https://audio.jukehost.co.uk/PqiGnmpciGheT4bouGACgW1DkC4GXhdV";
         songname = "Pani Da Rang";
     }
     else if (e.key == "q" || e.key == "Q") {
-        music.src = "https://audio.jukehost.co.uk/3W8v3XjvdgaC3nMzxfLOTilklEZgs9Sr";
+        music.src = "https://audio.jukehost.co.uk/oU62BFQ18genGP5iQeQkQTluOUQMTJ6Z";
         songname = "Quarter Past Midnight";
     }
     else if (e.key == "r" || e.key == "R") {
-        music.src = "https://audio.jukehost.co.uk/avJtChJPfzguT7EZgZWccGtGzRn5j7sF";
+        music.src = "https://audio.jukehost.co.uk/IUD463eJND9dhpT96XScyIQnH89UkzuN";
         songname = "Raaz Aankhein Teri";
     }
     else if (e.key == "s" || e.key == "S") {
-        music.src = "https://audio.jukehost.co.uk/xJp2pdkHHAUu9QmBHscZE5Ovsre6tKUx";
+        music.src = "https://audio.jukehost.co.uk/F9nDasMjQ29DUkQoGBDFMRyPRDFIFsAv";
         songname = "Sanu Ek Pal Chain";
     }
     else if (e.key == "t" || e.key == "T") {
-        music.src = "https://audio.jukehost.co.uk/oYI0Ab8Zj4uqC2eGD8dKHb3HlliAGZCT";
+        music.src = "https://audio.jukehost.co.uk/7eNO5tIwWB5VS7UvQDkRR50qdhFzwbJm";
         songname = "Tera Hone Laga Hoon";
     }
     else if (e.key == "u" || e.key == "U") {
-        music.src = "https://audio.jukehost.co.uk/jiEfV5HLM4urU076vSqYdo45KSfWazTM";
+        music.src = "https://audio.jukehost.co.uk/E1jPTV93d25yX7DgenkHObi1UYcqftmB";
         songname = "Udi Udi Jaye";
     }
     else if (e.key == "v" || e.key == "V") {
-        music.src = "https://audio.jukehost.co.uk/KgQa9yoJfKU6JqCRdcG7axzkjlaDt83e";
+        music.src = "https://audio.jukehost.co.uk/zZyBoLVsRroAuOwuHLlyK7JQrufHhfN0";
         songname = "Vacation";
     }
     else if (e.key == "w" || e.key == "W") {
-        music.src = "https://audio.jukehost.co.uk/HcbxU1Os5lSVsfSZ1G7cuAh0uhDnU9QV";
+        music.src = "https://audio.jukehost.co.uk/fEuObsOjzyxg7f9megCZpCD3Vp35PhIp";
         songname = "Wajah Tum Ho";
     }
     else if (e.key == "x" || e.key == "X") {
-        music.src = "https://audio.jukehost.co.uk/94qfBNosWQRTeXGLnOcIWWkivtp9L84z";
+        music.src = "https://audio.jukehost.co.uk/eQoOuQaTISx0gBidCXgerlfOH3Q30wnW";
         songname = "XStatic";
     }
     else if (e.key == "y" || e.key == "Y") {
-        music.src = "https://audio.jukehost.co.uk/QRu0O0S46GyySAqNkB1EkZU0ZAfnFFrV";
+        music.src = "https://audio.jukehost.co.uk/DAuuMCk4MxJPTyzDpMxTT845ehpBWjv9";
         songname = "Yaar Ni Milya";
     }
     else if (e.key == "z" || e.key == "Z") {
-        music.src = "https://audio.jukehost.co.uk/1ybyjkLiOSRAS5ZzO5Iyy9fKZ2b8twIZ";
+        music.src = "https://audio.jukehost.co.uk/fDBx9h4ssM9S1rUXfnPqwCTbp58C4WQC";
         songname = "Zara Zara Behekta Hai";
     }
     else if (e.key == " ") {
